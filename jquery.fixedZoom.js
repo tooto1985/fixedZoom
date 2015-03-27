057515d981386e0315fc97d9bcec580c33317865
tooto1985 <tooto1985@gmail.com>
2015-03-27 15:41:49 +0800

    new

---
+++ jquery.fixedZoom.js
@@ -0,0 +1,20 @@
+(function($) {
+  $.fn.fixedZoom = function() {
+    var _this = this;
+    $(window).resize(function() {
+      var _size;
+      if (!screen.deviceXDPI && !screen.logicalXDPI) {
+        _size = window.devicePixelRatio;
+      } else {
+        _size = screen.deviceXDPI / screen.logicalXDPI;
+      }
+      _this.each(function() {
+        $(this).css({
+          "zoom": 1 / _size,
+          "-moz-transform": "scale(" + 1 / _size + ")"
+        });
+      });
+    });
+    return this;
+  };
+})(jQuery);
