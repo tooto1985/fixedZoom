5b13f83a2d82c283a418ae5da90997a57ba0618e
tooto1985 <tooto1985@gmail.com>
2015-03-27 17:09:38 +0800

    modify

--- jquery.fixedZoom.js
+++ jquery.fixedZoom.js
@@ -1,20 +1,25 @@
 (function($) {
-  $.fn.fixedZoom = function() {
-    var _this = this;
-    $(window).resize(function() {
-      var _size;
-      if (!screen.deviceXDPI && !screen.logicalXDPI) {
-        _size = window.devicePixelRatio;
-      } else {
-        _size = screen.deviceXDPI / screen.logicalXDPI;
-      }
-      _this.each(function() {
-        $(this).css({
-          "zoom": 1 / _size,
-          "-moz-transform": "scale(" + 1 / _size + ")"
-        });
-      });
-    });
-    return this;
-  };
+	var sid=null;
+	$.fn.fixedZoom = function() {
+		var _this = this;
+		function run(_this) {
+			var _size;
+			if (!screen.deviceXDPI && !screen.logicalXDPI) {
+				_size = window.devicePixelRatio;
+			} else {
+				_size = screen.deviceXDPI / screen.logicalXDPI;
+			}
+			_this.each(function() {
+				$(this).css({
+					"zoom": 1 / _size,
+					"-moz-transform": "scale(" + 1 / _size + ")"
+				});
+			});
+		}
+		$(window).resize(function() {
+			sid !== null && clearTimeOut(sid);
+			sid = setTimeout(run,200);
+		});
+		return this;
+	};
 })(jQuery);
